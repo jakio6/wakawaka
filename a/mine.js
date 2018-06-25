8a754696fc78c999f557d70cb11810901340e283
yourname <yourmail@example.com>
2018-06-25 23:42:43 +0800

    重装

---
+++ a/mine.js
@@ -0,0 +1,15 @@
+function haha(){
+	var a=document.getElementById(me);
+	me.border-radius="20%";
+}
+
+
+function fn(a){
+	console.log(a);
+	var a = 123;
+	console.log(a);
+	function a(){}
+	console.log(a);
+	a = function b(){}
+	console.log(a);
+}
